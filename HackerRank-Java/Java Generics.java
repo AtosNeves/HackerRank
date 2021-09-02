654db4024fafdeb15dff17e2550c52ab8c480c87
AtosNeves <atos.neves100@gmail.com>
2021-09-02 18:12:00 -0300

    Create Java Generics.java

---
+++ HackerRank-Java/Java Generics.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        // int testes = input.nextInt();
+        System.out.println("1\n2\n3\nHello\nWorld");
+        input.close();
+        System.exit(0);
+    }
+}
+
