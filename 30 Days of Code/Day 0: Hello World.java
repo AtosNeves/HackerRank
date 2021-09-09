894792a3def4b4adbfd1254a638e71e1664d63e7
AtosNeves <atos.neves100@gmail.com>
2021-09-09 11:09:25 -0300

    Create Day 0: Hello World.java

---
+++ 30 Days of Code/Day 0: Hello World.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello, World.");
+        String word = input.nextLine();
+        System.out.println(word);
+        System.exit(0);
+ 
+    }
+ 
+}
