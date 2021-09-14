2dff6dcbf4dbfa86f6840e54d38b41491ae82eb7
AtosNeves <atos.neves100@gmail.com>
2021-09-14 10:57:50 -0300

    Update Day 5: Loops.java

--- 30 Days of Code/Day 5: Loops.java
+++ 30 Days of Code/Day 5: Loops.java
@@ -1,16 +1,35 @@
 import java.util.*;
 
+
 import java.io.IOException;
 
 class Solution {
     public static void main(String[] args) throws IOException {
 
         Scanner input = new Scanner(System.in);
-        int entrada = input.nextInt();
+        int entra = input.nextInt();
+        TabuadaCalculo tabuadinha = new TabuadaCalculo();
+        tabuadinha.resultado = entra;
+
         for (int i = 1; i <= 10; i++) {
-            System.out.println(entrada + " x " + i + " = " + entrada * i);
+
+            int mult = tabuadinha.retorno(tabuadinha.resultado);
+            
+            System.out.println(tabuadinha.resultado + " x " + i + " = " + tabuadinha.retorno(tabuadinha.resultado) * i);
+
         }
+    }
+}
+
+class TabuadaCalculo {
+
+    public static int resultado;
+
+    public int retorno(int entrada) {
+        resultado = entrada;
+        return resultado;
 
     }
+
 }
 
