1399abde80a11c61bec8d8245f9a5aeed47e9c14
Atos Neves <atos.neves100@gmail.com>
2021-10-01 22:23:25 -0300

    Create Day 16: Exceptions - String to Integer.java

---
+++ 30 Days of Code/Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.*;
+
+class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        String entrada = input.nextLine();
+        boolean veri = true;
+        int o = 0;
+        try {
+            
+            System.out.println(Integer.parseInt(entrada));
+            
+            
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+
+    }
+
+}
+
